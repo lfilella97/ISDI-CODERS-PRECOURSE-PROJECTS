15a1d203e073d0c4ff6c41667ecefbe2d89bf180
Lluís Filella <109596682+lfilella97@users.noreply.github.com>
2022-09-14 17:54:37 +0200

    Update and rename Calculadora PRO.html to Calculadora PRO.js

--- Calculadora PRO.js
+++ Calculadora PRO.js
@@ -1,44 +1,4 @@
-calculadora<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora</title>
-</head>
-<body>
-    <script>
-/*Haz una calculadora. Un único programa al que le pasarás dos parámetros y el usuario podrá visualizar por consola la suma, resta, multiplicación y división entre ambos números. 
-El resultado debería ser mostrado con 3 decimales como mucho (en caso de que hubieran).
-El programa debe gestionar y actuar correctamente (gestión de errores) en el caso de que el usuario introduzca cualquier cosa que no sean números.
 
-Si el usuario introduce un solo numero, deberá mostrar SOLO su raíz cuadrada, si vuelve a introducir los dos, volverá a mostrar las 4 operaciones de siempre.
-Si el usuario introduce una letra, debera mostrarle un aviso de que lo que ha introducido no es un número
-Los resultados deberían almacenarse dentro de una array y mostrarlos de una forma amigable al usuario.
-
-Podrías hacer que la calculadora realice operaciones sean cuales sean el número de argumentos pasados a la función?
-Documentación:
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
-function sum() {
-  let acc = 0;
-  for (num in arguments) {
-    console.log(num);
-    acc += arguments[num];
-  }
-  return acc;
-}
-sum(2, 3, 4); // acc=9
-Después de hacer todas las operaciones, el programa deberá preguntar al usuario si desea volver a realizar otra operación, volviendo a almacenar más resultados y mostrándolos.
-calculator(n1,n2)
-//Output => sum, subs, mult, div...
-prompt("New numbers? y/n")
-    Case "y" => calculator(n1,n2)
-                //Output => sum1, subs1, mult1, div1, sum2, subs2, mult2, div2...
-    Case "n" => "Bye!"
-
-*/
-//calculadora() para inicializar
 function calculadora(){
     let resultado = []
     do{
@@ -135,8 +95,4 @@ function operaciones(){
     return '\nEl resultado de las operaciones es: ' + sum() + ', ' + res() + ', ' + mult() + ', ' +  div()
 }
 
-
-
-    </script>
-</body>
-</html>
+calculadora()
