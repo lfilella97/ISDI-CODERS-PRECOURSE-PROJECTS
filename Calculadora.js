bf2740f5a3daf0336ab9f94c1aee5d0b3ee5c543
Lluís Filella <109596682+lfilella97@users.noreply.github.com>
2022-09-14 18:02:31 +0200

    Update and rename Calculadora.html to Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -1,21 +1,3 @@
-calculadora<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora</title>
-</head>
-<body>
-    <script>
-/*Haz una calculadora. Un único programa al que le pasarás dos parámetros y el usuario podrá visualizar por consola la suma, resta, multiplicación y división entre ambos números. 
-El resultado debería ser mostrado con 3 decimales como mucho (en caso de que hubieran).
-El programa debe gestionar y actuar correctamente (gestión de errores) en el caso de que el usuario introduzca cualquier cosa que no sean números.
-
-Si el usuario introduce un solo numero, deberá mostrar SOLO su raíz cuadrada, si vuelve a introducir los dos, volverá a mostrar las 4 operaciones de siempre.
-Si el usuario introduce una letra, debera mostrarle un aviso de que lo que ha introducido no es un número
-Los resultados deberían almacenarse dentro de una array y mostrarlos de una forma amigable al usuario.
-*/
 
 let digitOne = prompt('introduce el primer número:');
 let digitTwo = prompt('introduce el segundo número:');
@@ -72,8 +54,5 @@ function calculadora(){
 
     return console.log(result), resultArray
 }
-
 calculadora()
-    </script>
-</body>
-</html>
+  
